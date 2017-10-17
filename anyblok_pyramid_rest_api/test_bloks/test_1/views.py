d05d639e63acf6d51a854bc6431d5279bbf4f27d
jssuzanne <jssuzanne@anybox.fr>
2017-10-17 11:43:28 +0200

    fix test_crud_ressource

--- anyblok_pyramid_rest_api/test_bloks/test_1/views.py
+++ anyblok_pyramid_rest_api/test_bloks/test_1/views.py
@@ -4,6 +4,4 @@ from anyblok_pyramid_rest_api.crud_resource import CrudResource
 
 @resource(collection_path='/examples', path='/examples/{id}')
 class ExampleResource(CrudResource):
-
-    def __init__(self, request, context=None):
-        self.model = 'Model.Example'
+    model = 'Model.Example'
